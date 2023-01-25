9c421bf31a636e1fa372ee9f5c9695cb5d563f93
Dewashish Painkra <dewashishpainkra126@gmail.com>
2023-01-25 11:55:46 +0530

    final submission

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,26 @@
 package com.driver;
 
 public class Main {
-
+    public static class Product
+    {
+        public int product(int x,int y)
+        {
+            return x*y;
+        }
+        public int product(int x,int y,int z)
+        {
+            return x*y*z;
+        }
+        public double product(double x,double y)
+        {
+            return x*y;
+        }
+    }
+    public static void main(String [] args)
+    {
+        Product p=new Product();
+        System.out.println(p.product(10,30));
+        System.out.println(p.product(10,30,40));
+        System.out.println(p.product(10.1,30.1));
+    }
 }
